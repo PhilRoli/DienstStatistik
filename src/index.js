5c538d2c8ad482efe2bdf9d37861d828b6299d83
Philipp Rolinek <philipprolinek@gmail.com>
2023-01-07 17:08:30 +0100

    Added ServiceWorker to generate PWA and cache data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // CSS
 // Base
 import './style/App.css';
@@ -35,6 +36,8 @@ root.render(
         </HashRouter>
     </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
