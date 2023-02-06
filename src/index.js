ae30a693d6a70e0327780d00d4142a6ce52545b0
Philipp Rolinek <philipprolinek@gmail.com>
2023-02-06 23:09:15 +0100

    Change back to Browser Router?

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // CSS
 // Base
@@ -31,9 +31,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
